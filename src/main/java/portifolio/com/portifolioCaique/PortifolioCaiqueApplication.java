e7de6e177e2b74375c7b0ec02f9fdbca645350ba
Caique Junior da silva <139816879+CaiqueJrsilva@users.noreply.github.com>
2024-06-03 20:17:32 -0300

    Primeiro commit

---
+++ src/main/java/portifolio/com/portifolioCaique/PortifolioCaiqueApplication.java
@@ -0,0 +1,13 @@
+package portifolio.com.portifolioCaique;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PortifolioCaiqueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PortifolioCaiqueApplication.class, args);
+	}
+
+}
---
+++ src/test/java/portifolio/com/portifolioCaique/PortifolioCaiqueApplicationTests.java
@@ -0,0 +1,13 @@
+package portifolio.com.portifolioCaique;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PortifolioCaiqueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
